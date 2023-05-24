ad3b334bd38646954fac90587926d4befea61b98
Elias Breitenbach <elias.b@me.com>
2023-05-24 22:06:51 +0200

    Create a project in github

---
+++ Instagram-Login/Instagram-Login/Instagram_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_LoginApp.swift
+//  Instagram-Login
+//
+//  Created by Elias Breitenbach on 24.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
